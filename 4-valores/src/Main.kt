fe9d8293e8ed6106e5cf3c1336c9fe7cba82c464
Emily Santana <imemilysantana@gmail.com>
2024-09-13 13:11:46 -0300

    main.kt

--- 4-valores/src/Main.kt
+++ 4-valores/src/Main.kt
@@ -1,26 +1,26 @@
 fun main() {
-    // Cria uma lista para armazenar os valores
+    // criando uma lista para armazenar os valores
     val valores = mutableListOf<Double>()
 
-    // Solicita ao usuário que informe 4 valores
+    // peça ao usuário que informe 4 valores
     for (i in 1..4) {
-        print("Digite o $iº valor: ")
+        print("digita o $iº valor: ")
         val valor = readLine()?.toDoubleOrNull()
         if (valor != null) {
             valores.add(valor)
         } else {
-            println("Valor inválido. Tente novamente.")
+            println("valor inválido. tente novamente.")
             return
         }
     }
 
-    // Obtém o primeiro, o último e o maior valor
+    // coloque o primeiro, o último e o maior valor
     val primeiroValor = valores.first()
     val ultimoValor = valores.last()
     val maiorValor = valores.maxOrNull()
 
-    // Exibe os valores
-    println("O primeiro valor informado é: $primeiroValor")
-    println("O último valor informado é: $ultimoValor")
-    println("O maior valor informado é: $maiorValor")
+    // mostre os valores
+    println("o primeiro valor informado é: $primeiroValor")
+    println("o último valor informado é: $ultimoValor")
+    println("o maior valor informado é: $maiorValor")
 }
