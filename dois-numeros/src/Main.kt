6c3cee90368b855dce48e319932a26875d12829a
Emily Santana <imemilysantana@gmail.com>
2024-09-13 12:25:42 -0300

    Update Main.kt

--- dois-numeros/src/Main.kt
+++ dois-numeros/src/Main.kt
@@ -1,20 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    // Solicita ao usuário que informe o primeiro número
-    print("Digite o primeiro número: ")
+    // peça ao usuário que informe o primeiro número
+    print("digita o primeiro número: ")
     val numero1 = readLine()?.toDoubleOrNull()
 
-    // Solicita ao usuário que informe o segundo número
-    print("Digite o segundo número: ")
+    // peça ao usuário que informe o segundo número
+    print("digita o segundo número: ")
     val numero2 = readLine()?.toDoubleOrNull()
 
-    // Verifica se os números informados são válidos
+    // veja se os números informados são válidos
     if (numero1 != null && numero2 != null) {
-        // Determina e exibe o maior dos dois números
+        // determine e exiba o maior dos dois números
         val maiorNumero = if (numero1 > numero2) numero1 else numero2
-        println("O maior número é: $maiorNumero")
+        println("o maior número é: $maiorNumero")
     } else {
-        println("Um ou ambos os valores informados não são válidos.")
+        println("um ou outros os valores mostrados não são válidos.")
     }
 }
